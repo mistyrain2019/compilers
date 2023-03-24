b02a2c10d22a41ecb4a3bb510f3c5eea313d281f
lixinyang <741162208@qq.com>
2023-03-25 03:08:46 +0800

    initially implement cyk

--- app/src/main/scala/com/compilers/demo/parsing/CykAlgorithm.scala
+++ app/src/main/scala/com/compilers/demo/parsing/CykAlgorithm.scala
@@ -2,6 +2,8 @@ package com.compilers.demo.parsing
 
 import com.compilers.demo.parsing.CykAlgorithm.checkChomsky
 
+import scala.collection.mutable
+
 
 /**
  * can be used to check if a String lists belong to the language of a CFG
@@ -15,7 +17,36 @@ class CykAlgorithm(val cfg: ContextFreeGrammar):
   checkChomsky(this.cfg) // throw an exception if cfg is not in Chomsky Normal Form
 
   def accept(inputSymbols: List[String]): Boolean =
-    false
+    val n = inputSymbols.size
+
+    val r = Array.ofDim[mutable.Set[String]](n, n + 1)
+    for i <- 0 until n do
+      for j <- 0 to n do
+        r(i)(j) = mutable.Set[String]()
+
+    val size2Rules = cfg.rules.filter(_.rightHandSide.size == 2)
+    val size1Rules = cfg.rules.filter(_.rightHandSide.size == 1)
+
+    for i <- inputSymbols.indices do
+      if cfg.terminals.contains(inputSymbols(i)) then
+        for left <- size1Rules.filter(_.rightHandSide.head == inputSymbols(i)).map(_.leftHandSide) do
+          r(i)(1).add(left)
+
+
+    for j <- 2 to n do
+      for i <- 0 to (n - j) do
+        for k <- 1 until j do
+          val r1: mutable.Set[String] = r(i)(k)
+          val r2: mutable.Set[String] = r(i + k)(j - k)
+          for (rule <- size2Rules) do {
+            val leftNonTerminal = rule.rightHandSide(0)
+            val rightNonTerminal = rule.rightHandSide(1)
+            if (r1.contains(leftNonTerminal) && r2.contains(rightNonTerminal)) {
+              r(i)(j).add(rule.leftHandSide)
+            }
+          }
+
+    r(0)(n).contains(cfg.startSymbol)
 
 object CykAlgorithm:
 
@@ -43,3 +74,6 @@ object CykAlgorithm:
 
     if sizeTwoRhs.flatten.exists(GrammarUtil.isTerminal) then
       throw NonChomskyException("size 2 Rhs' containing terminals is not allowed in a Chomsky Normal Form!")
+
+    if rightHandSides.contains(cfg.startSymbol) then
+      throw NonChomskyException("size 2 Rhs' containing the start symbol is not allowed in a Chomsky Normal Form!")
--- app/src/test/scala/com/compilers/demo/parsing/TestCykParsingMethod.scala
+++ app/src/test/scala/com/compilers/demo/parsing/TestCykParsingMethod.scala
@@ -9,7 +9,7 @@ class TestCykParsingMethod extends AnyFunSuite {
 
   test("testing Chomsky Normal Form checking") {
     val startTime = System.currentTimeMillis()
-//    testChomsky()
+    //    testChomsky()
     testChomsky2()
     val endTime = System.currentTimeMillis()
     println(s"time used: ${endTime - startTime} ms")
@@ -45,11 +45,12 @@ class TestCykParsingMethod extends AnyFunSuite {
   private def testCykAccepting(): Unit =
     val rule1 = ContextFreeProductionRule("S -> A B")
     val rule2 = ContextFreeProductionRule("A -> 1")
-    val rule3 = ContextFreeProductionRule("B -> B C")
-    val rule4 = ContextFreeProductionRule("C -> D E")
-    val rule5 = ContextFreeProductionRule("D -> 3333")
-    val rule6 = ContextFreeProductionRule("E -> 2")
-    val cfg = ContextFreeGrammar("S", List(rule1, rule2, rule3, rule4, rule5))
+    val rule3 = ContextFreeProductionRule("B -> D C")
+    val rule4 = ContextFreeProductionRule("C -> E F")
+    val rule5 = ContextFreeProductionRule("D -> E F")
+    val rule6 = ContextFreeProductionRule("E -> 3333")
+    val rule7 = ContextFreeProductionRule("F -> 2")
+    val cfg = ContextFreeGrammar("S", List(rule1, rule2, rule3, rule4, rule5, rule6, rule7))
     val cyk = CykAlgorithm(cfg)
     val inputSymbols: List[String] = "1 3333 2 3333 2".split(" ").toList
     println(cyk.accept(inputSymbols))
